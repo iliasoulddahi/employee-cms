e57b6f4f840aff0ff940138708580fd0c88b5fa9
ilias ould dahi <65334020+iliasoulddahi@users.noreply.github.com>
2023-03-24 00:52:06 +0700

    feat: add table user & employee

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,13 @@
-const app = require('express')()
-const router = require('./routes')
+const express = require('express')
+const app = express()
+// const router = require('./routes')
 const cors = require('cors')
 const port = process.env.PORT || 3000
 
 app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-app.use(router)
-
+// app.use(router)
+app.get('/', (req,res)=> res.send('Active'))
 
 app.listen(port, ()=> console.log(`Listening To Port ${port}`))
